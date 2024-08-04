f8d401b62be9de4c3b68530ccf47974a841c3ed3
Flavia-20 <flaviamonique38@gmail.com>
2024-08-04 13:42:37 -0300

    Instaciando o Objeto Filme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso Chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+
+        System.out.println(meuFilme.nome);
+
         
     }
 }
